2ebd689c8c6ce7af0819885f3857ab5fa0704711
Marco Brugin <marcobrugin00@gmail.com>
2022-12-08 23:01:23 +0100

    Fix errori

--- Creazione_Tabelle.sql
+++ Creazione_Tabelle.sql
@@ -1,5 +1,6 @@
 drop table if exists Valutazione;
 drop table if exists Credenziale;
+drop table if exists Iscrizione;
 drop table if exists Utente;
 drop table if exists CorsodiStudio;
 drop table if exists ClassediLaurea;
@@ -14,38 +15,38 @@ regione varchar(16) not null
 
 create table ClassediLaurea (
 num_classe char(4) primary key,
-denominazione varchar(30) not null,
+denominazione text not null,
 illustrazione text,
 area_disciplinare varchar(30) not null,
-gruppo_disciplinare varchar(30) not null,
+gruppo_disciplinare varchar(50) not null,
 durata ENUM('triennale', 'magistrale', 'a ciclo unico') not null
 );
 
 create table CorsodiStudio (
 ateneo varchar(50),
 classe_laurea char(4),
-nome varchar(50) not null,
+nome varchar(150) not null,
 PRIMARY KEY(ateneo, classe_laurea, nome),
-accesso ENUM('libero', 'chiuso', 'programmato') not null,
-link varchar(30) not null,
+accesso ENUM('Accesso programmato','Accesso libero con prova','Accesso a numero chiuso','Accesso libero cronologico') not null,
+link text not null,
 foreign key (ateneo) references Ateneo(nome),
 foreign key (classe_laurea) references ClassediLaurea(num_classe)
 );
 
 create table Utente (
-nome_utente varchar(20) primary key,
+nome_utente varchar(40) primary key,
 nome varchar(20) not null,
 cognome varchar(30) not null,
 data_nascita date not null,
-genere ENUM('M','F') not null,
+genere ENUM('M','F','ND') not null,
 scuola_sup ENUM('tecnico industriale','tecnico commerciale','scientifico','linguistico','classico') not null
 );
 
 create table Valutazione (
-nome_utente varchar(20),
+nome_utente varchar(40),
 classe_laurea char(4),
 PRIMARY KEY(nome_utente,classe_laurea),
-data date not null,
+datav date not null,
 commento varchar(200) not null,
 tag ENUM('1','2') not null,
 p_complessivo int not null,
@@ -62,5 +63,16 @@ pw varchar(10),
 data_inserimento date, 
 utente varchar(20),
 PRIMARY KEY(pw,data_inserimento,utente),
-FOREIGN KEY(utente) REFERENCES Utente(email) ON UPDATE CASCADE ON DELETE CASCADE
+FOREIGN KEY(utente) REFERENCES Utente(nome_utente) ON UPDATE CASCADE ON DELETE CASCADE
+);
+create table Iscrizione(
+    ateneo varchar(50),
+    classe char(4), 
+    corso varchar(150), 
+    nome_utente varchar(40),
+    PRIMARY KEY(ateneo,classe,corso), 
+    punteggio_scuola_provenienza int not null, 
+    datai date not null, 
+    dataf date not null,
+    FOREIGN KEY(ateneo,classe,corso) REFERENCES CorsodiStudio(ateneo, classe_laurea, nome) ON UPDATE CASCADE ON DELETE CASCADE
 );
