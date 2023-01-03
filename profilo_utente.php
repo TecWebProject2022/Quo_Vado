1c895a72abacf56f97828b0a7be1b6ca85d66429
Marco Brugin <marcobrugin00@gmail.com>
2023-01-03 19:43:29 +0100

    Sessione scaduta

--- profilo_utente.php
+++ profilo_utente.php
@@ -4,9 +4,9 @@ echo $_SESSION['user'];
 echo time()-$_SESSION['time'];
 // Se non hai fatto il login o la tua sessione (durata max 1 h di inattività) è scaduta
 if(!isset($_SESSION['user']) || time()-$_SESSION['time']>3600){
-    $_SESSION=array(); 
-    session_destroy();
-    $GLOBALS['sessione']='<p>Sessione scaduta</p>';
+    unset($_SESSION['user']); 
+    unset($_SESSION['time']);
+    $_SESSION['sessione']='<p>Sessione Scaduta</p>';
     header('Location:login.php');
 }
 ?>
