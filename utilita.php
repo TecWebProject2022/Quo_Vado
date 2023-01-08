26b2bea6fba30d7bc48f521133b407970e59fdb1
Matteo-Umatche <matteoumatche@gmail.com>
2023-01-08 16:20:45 +0100

    ...

--- utilita.php
+++ utilita.php
@@ -5,4 +5,9 @@ function PulisciInput($value){
     #$value=htmlentities($value); #traduce tag html con i codice relativi-> prende tutto come carattri se inverto non trovo nulla
     return $value;
 }
+
+function filterSql($string) {
+    $string = preg_replace('/(SELECT|UPDATE|DELETE|INSERT|WHERE|FROM|TRUNCATE|DROP|SHOW|TABLE|USE|DATABASE|LOAD DATA|INFILE|INTO TABLE|DESCRIBE|EXPLAIN|AND|OR|UNION|CREATE|ALTER|DROP|RENAME|BACKUP|RESTORE|GRANT|REVOKE|LOCK|UNLOCK|SET)/i', '', $string);
+    return $string;
+}
 ?>
