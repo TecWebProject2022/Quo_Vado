bdbb7e498ac4bcbd7dcfaa6a1b340b85e485a5ee
Matteo-Umatche <matteoumatche@gmail.com>
2023-01-09 19:59:29 +0100

    bugfixes

--- js/registration.js
+++ js/registration.js
@@ -1,60 +1,61 @@
 //mega giga bozza sto solo cercando di capire js
-
-// se l' input e' valido ritorna true altrimenti ritorna false e aggiunge un messaggio di errore
-function Validate(element){
-  //controllo se all' interno del tag spaaaaaaaaan in cui e contenuto l' input esiste un altro tag, se esiste 
-  //significa che esiste un precedente messaggio di errore, che rimuovo
+function displayMessage(element){
   var parent= element.parentNode;
-  if(parent.children.length==2){
-      parent.removeChild(parent.children[1]);
-  }
-
-  if(element.validity.patternMismatch || element.validity.tooLong || element.validity.tooShort || element.validity.valueMissing){
-    
-    var a=document.createElement('strong');
-    error= element.validity.valueMissing ? element.dataset.msgEmpty:element.dataset.msgInvalid;
-    a.appendChild(document.createTextNode(error));
-    parent.appendChild(a);
-    element.focus();
-    element.select();
-    return false; 
-  }
-  
-  return true;
-} 
-
-
-function dateValidate(element){
-  var input=Date.parse(element.value);
-  var now= new Date();
-  if(element.checkValidity() && (input >= Date.parse(element.getAttribute('min')))){
-    if(input.getFullYear() <= now.getFullYear()){
-      if(input.getMonth()<= now.getMonth()){
-        if(input.getDate()<=now.getDate()){
-          return true;
-        }
-      }
-    }
-    var a=document.createElement('strong');
-    a.appendChild(document.createTextNode(element.dataset.msgInvalid));
-    parent.appendChild(a);
-    element.focus();
-    element.select();
-  }
   var a=document.createElement('strong');
-  a.appendChild(document.createTextNode(element.dataset.msgEmpty));
+  error= element.validity.valueMissing ? element.dataset.msgEmpty:element.dataset.msgInvalid;
+  a.appendChild(document.createTextNode(error));
   parent.appendChild(a);
   element.focus();
   element.select();
-  return false;
+}
+// se l' input e' valido ritorna true altrimenti ritorna false e aggiunge un messaggio di errore
+function Validate(element){
+  var parent= element.parentNode;
+  if(parent.children.length==2){
+        parent.removeChild(parent.children[1]);
+    }
+  if(element.validity.patternMismatch || element.validity.tooLong || element.validity.tooShort || element.validity.valueMissing){
+    displayMessage(element);    
+    return false; 
+  }
+    return true;
+} 
+
+
+function dateValidate(element) {
+  var parent= element.parentNode;
+  if(parent.children.length==2){
+        parent.removeChild(parent.children[1]);
+    }
+  if (!(element.validity.patternMismatch && element.validity.rangeUnderflow && element.validity.valueMissing)) {
+    var input = new Date(element.value);
+    var now = new Date();
+    if (input > now) {
+      displayMessage(element);
+      return false;
+    }
+  } else {
+    displayMessage(element);
+    return false;
+  }
+  return true;
 }
 
 // controlla che il valore inserito sia valido e uguale a quello inserito nel campo password
 function r_PasswordValidate(element){
+  var parent= element.parentNode;
+  if(parent.children.length==2){
+        parent.removeChild(parent.children[1]);
+    }
   if(element.value == document.getElementById("password").value) return Validate(element);
   else{
+    var parent= element.parentNode;
+    if(parent.children.length==2){
+          parent.removeChild(parent.children[1]);
+    }
     var a=document.createElement('strong');
-    a.appendChild(document.createTextNode("Le password non coincidono"));
+    error= element.validity.valueMissing ? element.dataset.msgEmpty:element.dataset.msgInvalid;
+    a.appendChild(document.createTextNode("le password non coincidono"));
     parent.appendChild(a);
     element.focus();
     element.select();
