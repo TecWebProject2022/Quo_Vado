8851e66793846680cc9eb8a68e99be0dc4d06412
Marco Brugin <marcobrugin00@gmail.com>
2023-01-13 14:17:15 +0100

    Fix

--- js/utilita.js
+++ js/utilita.js
@@ -1,7 +1,6 @@
 function Conferma_eliminazione(){
 	
-	var domanda = confirm("Sei sicuro di voler cancellare?");
-	
+	var domanda = confirm("Sei sicuro di voler cancellare i commenti selezionati?","Eliminazione commento!!");
 	if (domanda){
 		return true;
 	}
