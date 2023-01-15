9c3449af99a934b2936ceaca32507a755adcb51c
Marco Brugin <marcobrugin00@gmail.com>
2023-01-15 23:36:46 +0100

    Correzione menu

--- js/area_utente.js
+++ js/area_utente.js
@@ -1,3 +1,11 @@
 function OnDelete(){
 return window.confirm("Sei sicuro di voler elminirare l'elemento selezionato?")
 }
+function OnInsert(){
+    var commento= document.getElementById('commento');
+    console.log(commento.value.length);
+    if(commento.value.length<10 || commento.value.length>200 )
+    console.log("hh");
+    return false;
+}
+ 
