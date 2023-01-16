5b20034f39fd82243d7cdf48a803f40325846978
Matteo-Umatche <matteoumatche@gmail.com>
2023-01-17 00:33:02 +0100

    area admin

--- area_admin.php
+++ area_admin.php
@@ -5,13 +5,13 @@ session_start();
 if(!isset($_SESSION['user']) || !isset($_SESSION['time']) || time()-$_SESSION['time']>3600){
     unset($_SESSION['user']); 
     unset($_SESSION['time']);
-    $_SESSION['sessione']='<p>Sessione Scaduta</p>';
+    $_SESSION['sessione']='<p class="error">Sessione Scaduta</p>';
     header('Location:login.php');
 }
 else if($_SESSION['user']!='admin'){
     unset($_SESSION['user']); 
     unset($_SESSION['time']);
-    $_SESSION['sessione']='<p>Non hai i permessi di admin per accedere a quest\' area.</p>';
+    $_SESSION['sessione']='<p class="error">Non hai i permessi di admin per accedere a quest\' area.</p>';
     header('Location:login.php');
 }
 
@@ -37,10 +37,17 @@ if($dbOK){
         # in base ai campi inseriti imposto la query
         $user=isset($_POST['com_utente'])?pulisciInput($_POST['com_utente']):'';
         $classe=isset($_POST['com_classe'])?pulisciInput($_POST['com_classe']):'';
+
+        if (($user?(!preg_match('/^[@a-zA-Z0-9._-]{4,40}$/',$user)): false)){
+            $msgCommenti.='<li class="error">Il campo username non può contenere spazi e deve contenere da 4 a 40 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - )</li>';
+        }
+        if (($classe?(!preg_match('/^(L|LM)[0-9]{2}$/',$classe)): false)){
+            $msgCommenti.='<li class="error">La classe di laurea non puo\' contenere spazi e vanno dalla classe L01 alla L43 e dalla LM01 alla LM94</li>';
+        }
+
         if($msgCommenti){
             $msgCommenti.='<ul>'.$msgCommenti.'</ul>';
-        }
-        else{
+        }else{
             if($user || $classe ){
                 $query_commenti='SELECT nome_utente,datav,classe_laurea,tag,commento,p_complessivo,p_acc_fisica, p_servizio_inclusione,tempestivita_burocratica, p_insegnamento FROM Valutazione WHERE ';
                 $query_commenti.= $user? "nome_utente='".$user."'" : '';
@@ -55,8 +62,8 @@ if($dbOK){
                 if($commenti=$db->ExecQueryNum($query_commenti)){
                     $formCommenti='<form id="formEliminaCommenti" action="area_admin.php" method="post"><fieldset><legend>Seleziona i commenti da eliminare</legend>';
                     for($i=0;$i<count($commenti);$i++){
-                        $commento='<span>'.$commenti[$i][0].'|'.$commenti[$i][2].'|'.date("d-m-Y",strtotime($commenti[$i][1])).':';
-                        $commento.='<p>'.$commenti[$i][4].'</p>';
+                        $commento='<span> utente: '.$commenti[$i][0].'| classe di laurea: '.$commenti[$i][2].'| data commento:'.date("d-m-Y",strtotime($commenti[$i][1]));
+                        $commento.='<p>commento :'.$commenti[$i][4].'</p>';
                         $commento.='<dl>
                             <dt>Punteggio complessivo:</dt><dd> '.$commenti[$i][5].' </dd>
                             <dt>Punteggio accessibilita fisica:</dt><dd> '.$commenti[$i][6].' </dd>
@@ -70,11 +77,11 @@ if($dbOK){
                     }
                     $formCommenti.= '<input type="submit" id="delete_commento" name="delete_commento" value="elimina commenti selezionati"/></fieldset></form>';
                 }else{
-                    $msgCommenti.='<p>nessun commento</p>';
+                    $msgCommenti.='<p class="error">nessun commento</p>';
                 }
             }else{
                 #nessun valore inserito nel form perla ricerca
-                $msgCommenti.='<p>riempire almeno uno dei due campi</p>';
+                $msgCommenti.='<p class="error">riempire almeno uno dei due campi</p>';
             } 
         }  
     }
@@ -88,13 +95,13 @@ if($dbOK){
                 $userdata=explode("-",$i);
                 $query_delete_commenti="DELETE FROM Valutazione Where nome_utente=\"".$userdata[0]."\" && classe_laurea=\"".$userdata[1]."\" && tag=\"".$userdata[2]."\";";
                 if(!$db->Insert($query_delete_commenti)){
-                    $msgCommenti_delete.='<li>Si è verificato un errori ai nostri servizi, commento dell\'utente '.$userdata[0].' non eliminato</li>';
+                    $msgCommenti_delete.='<li class="error">Si è verificato un errori ai nostri servizi, commento dell\'utente '.$userdata[0].' non eliminato</li>';
                 }else{
-                    $msgCommenti_delete.='<li>Commento dell\'utente '.$userdata[0].' eliminato con successo</li>';
+                    $msgCommenti_delete.='<li class="error">Commento dell\'utente '.$userdata[0].' eliminato con successo</li>';
                 }
             }
         }else{
-            $msgCommenti_delete.='<li>Selezionare almeno un commento</li>';
+            $msgCommenti_delete.='<li class="error">Selezionare almeno un commento</li>';
         }
         $msgCommenti_delete.='</ul>';
     }
@@ -107,14 +114,28 @@ if($dbOK){
         $link=isset($_POST['cor_link'])?pulisciInput($_POST['cor_link']):'';
         $accesso=isset($_POST['cor_accesso'])?pulisciInput($_POST['cor_accesso']):'';
         #controlli sulle variabili
-
+        if (!preg_match('/^(L|LM)[0-9]{2}$/',$classe)){
+            $msgCorso.='<li class="error">La classe di laurea non puo\' contenere spazi e vanno dalla classe L01 alla L43 e dalla LM01 alla LM94</li>';
+        }
+        if (!preg_match('/^(Accesso programmato|Accesso libero con prova|Accesso a numero chiuso|Accesso libero cronologico)$/',$classe)){
+            $msgCorso.='<li class="error">Le modalita di accesso sono Accesso programmato,Accesso libero con prova,Accesso a numero chiuso,Accesso libero cronologico</li>';
+        }
+        if (!preg_match('/^[a-zA-Z\s]+$/',$ateneo)){
+            $msgCorso.='<li class="error">Il nome dell\'ateneo non puo\' contenere numeri o caratteri speciali<li>';
+        }
+        if (!preg_match('/^[a-zA-Z\s]+$/',$nome)){
+            $msgCorso.='<li class="error">Il nome del corso di laurea non puo\' contenere numeri o caratteri speciali<li>';
+        }
+        if(filter_var($link, FILTER_VALIDATE_URL)){
+            $msgCorso.='<li class="error">Formato del ink non valido, inserire un url corretto</li>';
+        }
         if(!$msgCorso){
             # tutte le variabili sono istanziate e valide
             $query_insert_corso="INSERT INTO CorsodiStudio(ateneo,classe_laurea,nome,accesso,link) VALUES ('".$ateneo."','".$classe."','".$nome."','".$accesso."','".$link."');";
             if($db->Insert($query_insert_corso)){
-                $msgCorso.='<p>'.$nome.' aggiunto con successo</p>';
+                $msgCorso.='<p class="error">'.$nome.' aggiunto con successo</p>';
             }else{
-                $msgCorso.='<p>Inserimento di '.$nome.' non riuscito, riprova</p>';
+                $msgCorso.='<p class="error">Inserimento di '.$nome.' non riuscito, riprova</p>';
             }
         }else{
             $msgCorso.='<ul>'.$msgCorso.'</ul>';
@@ -124,14 +145,24 @@ if($dbOK){
             $classe=isset($_POST['cor_classe'])?pulisciInput($_POST['cor_classe']):'';
             $ateneo=isset($_POST['cor_ateneo'])?pulisciInput($_POST['cor_ateneo']):'';
             $nome=isset($_POST['cor_nome'])?pulisciInput($_POST['cor_nome']):'';
-             #controlli sulle variabili
+            #controlli sulle variabili
+            if (!preg_match('/^(L|LM)[0-9]{2}$/',$classe)){
+                $msgCorso.='<li class="error">La classe di laurea non puo\' contenere spazi e vanno dalla classe L01 alla L43 e dalla LM01 alla LM94</li>';
+            }
+            if (!preg_match('/^[a-zA-Z\s]+$/',$ateneo)){
+                $msgCorso.='<li class="error">Il nome dell\'ateneo non puo\' contenere numeri o caratteri speciali<li>';
+            }
+            if (!preg_match('/^[a-zA-Z\s]+$/',$nome)){
+                $msgCorso.='<li class="error">Il nome del corso di laurea non puo\' contenere numeri o caratteri speciali<li>';
+            }
+
             if(!$msgCorso){
                 # tutte le variabili sono istanziate e valide
                 $query_delete_corso="DELETE FROM CorsodiStudio WHERE  ateneo='".$ateneo."' AND classe_laurea='".$classe."' AND nome='".$nome."';";
                 if($db->Insert($query_delete_corso)){
-                    $msgCorso.='<p>'.$nome.' rimosso con successo</p>';
+                    $msgCorso.='<p class="error">'.$nome.' rimosso con successo</p>';
                 }else{
-                    $msgCorso.='<p>Cancellazione di '.$nome.' non riuscita, riprova</p>';
+                    $msgCorso.='<p class="error">Cancellazione di '.$nome.' non riuscita, riprova</p>';
                 }
             }else{
                 $msgCorso.='<ul>'.$msgCorso.'</ul>';
@@ -147,35 +178,35 @@ if($dbOK){
         $rep=PulisciInput($_POST['repepassword']);
         
         if (!preg_match('/^[@a-zA-Z0-9._-]{4,20}$/',$vecchia)){
-            $msgPassword.='<li>Il campo vecchia password non può essere vuoto e non può contenere spazi e deve contenere da 4 a 20 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - )</li>';
+            $msgPassword.='<li class="error">Il campo vecchia password non può essere vuoto e non può contenere spazi e deve contenere da 4 a 20 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - )</li>';
         }
         if (!preg_match('/^[@a-zA-Z0-9._-]{4,20}$/',$nuova)){
-            $msgPassword.='<li>Il campo nuova password non può essere vuoto e non può contenere spazzi e deve contenere da 4 a 20 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - )</li>';
+            $msgPassword.='<li class="error">Il campo nuova password non può essere vuoto e non può contenere spazzi e deve contenere da 4 a 20 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - )</li>';
         }
         if($nuova!=$rep){
-            $msgPassword.='<li>Il campo nuova password e ripeti la password non corrispondono</li>';
+            $msgPassword.='<li class="error">Il campo nuova password e ripeti la password non corrispondono</li>';
         }
         
         if(!$msgPassword){
             $query="Select * from Credenziale where utente='admin' && pw=\"".$nuova."\";";
             if($r=$db->ExecQueryAssoc($query)){
-                $msgPassword.="<strong>Password già usata</strong>";
+                $msgPassword.="<p class="error">Password già usata</strong>";
             }else{
                 $query_controllo_pw="Select * from Credenziale where utente='admin' && pw=\"".$vecchia."\";";
                 if($db->ExecQueryAssoc($query_controllo_pw)){
                     $query_update_pw="UPDATE Credenziale SET attuale=0 WHERE utente='admin' and pw=\"".$vecchia."\";";
                     $query_update_pw.="INSERT INTO Credenziale(pw, data_inserimento, utente, attuale) VALUES('".$nuova."','".date('Y-m-d')."','admin',1);";
                     if($db->multiInsert($query_update_pw)){
-                        $msgPassword.="<strong>Password modificata con successo</strong>";
+                        $msgPassword.="<p class="error">Password modificata con successo</strong>";
                     }else{
-                        $msgPassword.="<strong>Cambiamento password non riuscito, i sistemi sono al momentamentamnete non disponibili</strong>";
+                        $msgPassword.="<p class="error">Cambiamento password non riuscito, i sistemi sono al momentamentamnete non disponibili</strong>";
                     } 
                 }else{
-                    $msgPassword.="<li>la vecchia password inserita non corrisponde</li>";
+                    $msgPassword.='<p class="error">la vecchia password inserita non corrisponde</strong>';
                 }
             }
         }else{
-            $msgPassword.="<strong><ul>".$msgPassword."</ul></strong>";   
+            $msgPassword.="<ul>".$msgPassword."</ul>";   
         }
     }
     $db->Disconnect();
