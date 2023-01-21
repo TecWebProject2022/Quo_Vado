7978265d3273c06882c6b2f4ae32777a156f7c21
Nevia23 <bzaghetto23@gmail.com>
2023-01-21 21:35:04 +0100

    admin

--- area_admin.php
+++ area_admin.php
@@ -32,47 +32,48 @@ $tags=array(
 
 $formSelezioneCommenti='
 <h2 class="titles_area_classi">Cancellazione commenti</h2>
-<form id="formTrovaCommenti" action="area_admin.php" method="post" onsubmit="return OnCommentFind(event)">
-    <fieldset>
-        <legend>Trova i commenti da eliminare</legend>
-        <label for="com_utente">Utente:</label>
-        <span><input id="com_utente" name="com_utente" type="text" 
-            data-msg-invalid="Il campo username non può contenere spazi e deve contenere da 4 a 40 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - )"
-            data-msg-empty=""/></span>
+    <form id="formTrovaCommenti" action="area_admin.php" method="post" onsubmit="return OnCommentFind(event)">
+        <fieldset>
+            <legend class="field_legend">Trova i commenti da eliminare</legend>
+            <label for="com_utente">Utente: 
+            <span><input id="com_utente" name="com_utente" type="text" 
+                data-msg-invalid="Il campo username non pu&ograve; contenere spazi e deve contenere da 4 a 40 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - )"
+                data-msg-empty=""/></span></label>
 
-        <label for="com_classe">Classe di laurea:</label>
-        <span><input id="com_classe" name="com_classe" type="text" 
-            data-msg-invalid="La classe di laurea non puo\' contenere spazi. Le classi di laurea vanno dalla classe L01 alla L43 e dalla LM01 alla LM94"
-            data-msg-empty=""/></span>
+            <label for="com_classe">Classe di laurea: 
+            <span><input id="com_classe" name="com_classe" type="text" 
+                data-msg-invalid="La classe di laurea non pu&ograve; contenere spazi. Le classi di laurea vanno dalla classe L01 alla L43 e dalla LM01 alla LM94"
+                data-msg-empty=""/></span></label>
 
-        <input type="submit" class="submit"  name="trova" value="trova"/>
-    </fieldset>
-</form>';
+            <input type="submit" class="submit"  name="trova" value="trova"/>
+            <input type="reset" name="reset" value="cancella tutto"/>
+        </fieldset>
+    </form>';
 
 $formGestioneCorsi='
 <h2 class="titles_area_classi">Gestione corsi di studio</h2>
-<p class="formdesc">Per aggiungere un corso di studi e necessario riempire tutti i campi, per eliminarne uno bastano nome, classe di laurea e ateneo.</p>
+<p class="formdesc">Per aggiungere un corso di studi &egrave; necessario riempire tutti i campi, per eliminarne uno bastano nome, classe di laurea e ateneo.</p>
 <form id="formCorsi" action="area_admin.php" method="post" >
     <fieldset>
-        <legend>Aggiungi o elimina un corso di studi</legend>
+        <legend class="field_legend">Aggiungi o elimina un corso di studi</legend>
         
-        <label for="cor_classe">Classe di laurea:</label>
+        <label for="cor_classe">Classe di laurea: 
         <span><input id="cor_classe" name="cor_classe" type="text" 
-            data-msg-invalid="La classe di laurea non puo\' contenere spazi.Le classi di laurea vanno dalla classe L01 alla L43 e dalla LM01 alla LM94"
-            data-msg-empty="La classe di laurea non puo essere vuota"/></span>
-        <label for="cor_ateneo">Ateneo:</label>
+            data-msg-invalid="La classe di laurea non pu&ograve; contenere spazi.Le classi di laurea vanno dalla classe L01 alla L43 e dalla LM01 alla LM94"
+            data-msg-empty="La classe di laurea non puo essere vuota"/></span></label>
+        <label for="cor_ateneo">Ateneo: 
         <span><input id="cor_ateneo" name="cor_ateneo" type="text" 
-            data-msg-invalid="Il nome dell\'ateneo non puo\' contenere numeri o caratteri speciali"
-            data-msg-empty="Il nome dell\'ateneo non puo essere vuoto"/></span>
-        <label for="cor_nome">Nome:</label>
+            data-msg-invalid="Il nome dell\'ateneo non pu&ograve; contenere numeri o caratteri speciali"
+            data-msg-empty="Il nome dell\'ateneo non puo essere vuoto"/></span></label>
+        <label for="cor_nome">Nome: 
         <span><input id="cor_nome" name="cor_nome" type="text" 
-            data-msg-invalid="Il nome del corso di laurea non puo\' contenere numeri o caratteri speciali"
-            data-msg-empty="il campo nome non puo essere vuoto"/></span>
-        <label for="cor_link">Link:</label>
+            data-msg-invalid="Il nome del corso di laurea non pu&ograve; contenere numeri o caratteri speciali"
+            data-msg-empty="il campo nome non puo essere vuoto"/></span></label>
+        <label for="cor_link">Link: 
         <span><input id="cor_link" name="cor_link" type="url" 
-            data-msg-invalid="Il link del corso non e nel formato corretto"
-            data-msg-empty="il campo link non puo essere vuoto"/></span>
-        <label for="cor_accesso">Accesso:</label>
+            data-msg-invalid="Il link del corso non &egrave; nel formato corretto"
+            data-msg-empty="il campo link non puo essere vuoto"/></span></label>
+        <label for="cor_accesso">Accesso: 
         <span><select name="cor_accesso" id="cor_accesso" 
             data-msg-invalid="Le modalita di accesso sono Accesso programmato,Accesso libero con prova,Accesso a numero chiuso,Accesso libero cronologico"
             data-msg-empty="il campo accesso non puo essere vuoto">
@@ -81,10 +82,11 @@ $formGestioneCorsi='
                 <option value="Accesso libero con prova">Accesso libero con prova</option>
                 <option value="Accesso a numero chiuso">Accesso a numero chiuso</option>
                 <option value="Accesso libero cronologico">Accesso libero cronologico</option>
-        </select></span>
+        </select></span></label>
 
         <input type="submit"  class="submit"  id="add_corso" name="add_corso" value="Aggiungi" onclick="return OnCourseAdd(event)"/>
         <input type="submit"  class="submit"  id="delete_corso" name="delete_corso" value="Elimina" onclick="return OnCourseDelete(event)"/>
+        <input type="reset" id="reset" name="reset" value="Cancella tutto"/>
     </fieldset>
 </form>';
 
@@ -92,19 +94,20 @@ $formCambioPw='
 <h2 id="CambioPw" class="titles_area_classi">Cambia Password</h2>
 <form id="form_passw" action="area_admin.php" method="post" >
     <fieldset>
-        <legend>Cambio password</legend>
-        <label for="oldpassword"><span lang="en">Immetti la tua vecchia Password: </span></label>
+        <legend class="field_legend">Cambio password</legend>
+        <label for="oldpassword"><span lang="en">Immetti la tua vecchia Password: </span>
         <span><input  value="<old>" type="password" id="oldpassword" name="Vecchiapassword" placeholder="Immetti la tua vecchia Password" maxlength="20"                      
-            data-msg-invalid="Il campo password non può contenere spazzi e deve contenere da 4 a 20 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - ), controlla e riprova"
-            data-msg-empty="Il campo vecchia password non può essere vuoto" /></span>
-        <label for="newpassword"><span lang="en">Immetti la tua nuova Password: </span></label>
+            data-msg-invalid="Il campo password non pu&ograve; contenere spazzi e deve contenere da 4 a 20 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - ), controlla e riprova"
+            data-msg-empty="Il campo vecchia password non pu&ograve; essere vuoto" /></span></label>
+        <label for="newpassword"><span lang="en">Immetti la tua nuova Password: </span>
         <span><input  value="<new>" type="password" id="newpassword" name="newpassword" placeholder="Immetti la tua nuova password" maxlength="20"                      
-            data-msg-invalid="Il campo password non può contenere spazzi e deve contenere da 4 a 20 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - ), controlla e riprova"
-            data-msg-empty="Il campo nuova password non può essere vuoto" /></span>
-        <label for="repeat"><span lang="en">Ripeti la Password: </span></label>
+            data-msg-invalid="Il campo password non pu&ograve; contenere spazzi e deve contenere da 4 a 20 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - ), controlla e riprova"
+            data-msg-empty="Il campo nuova password non pu&ograve; essere vuoto" /></span></label>
+        <label for="repeat"><span lang="en">Ripeti la Password: </span>
         <span><input  value="" type="password" id="repeat" name="repepassword" placeholder="Ripeti la password" maxlength="20"                      
-            data-msg-empty="Il campo repeti password non può essere vuoto" /></span>   
+            data-msg-empty="Il campo repeti password non pu&ograve; essere vuoto" /></span></label>   
         <input type="submit"  class="submit" id="submit" name="salva" value="Salva"/>
+        <input type="reset" id="reset" name="reset" value="Cancella tutto"/>
     </fieldset>
 </form>';
 
@@ -119,10 +122,10 @@ if($dbOK){
         $classe=isset($_POST['com_classe'])?pulisciInput($_POST['com_classe']):'';
 
         if (($user?(!preg_match('/^[@a-zA-Z0-9._-]{4,40}$/',$user)): false)){
-            $msgCommenti.='<li class="error">Il campo username non può contenere spazi e deve contenere da 4 a 40 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - )</li>';
+            $msgCommenti.='<li class="error">Il campo username non pu&ograve; contenere spazi e deve contenere da 4 a 40 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - )</li>';
         }
         if (($classe?(!preg_match('/^(L|LM)[0-9]{2}$/',$classe)): false)){
-            $msgCommenti.='<li class="error">La classe di laurea non puo\' contenere spazi.Le classi di laurea vanno dalla classe L01 alla L43 e dalla LM01 alla LM94</li>';
+            $msgCommenti.='<li class="error">La classe di laurea non pu&ograve; contenere spazi.Le classi di laurea vanno dalla classe L01 alla L43 e dalla LM01 alla LM94</li>';
         }
 
         if($msgCommenti){
@@ -179,7 +182,7 @@ if($dbOK){
                 $userdata=explode("-",$i);
                 $query_delete_commenti="DELETE FROM Valutazione Where nome_utente=\"".$userdata[0]."\" && classe_laurea=\"".$userdata[1]."\" && tag=\"".$userdata[2]."\";";
                 if(!$db->Insert($query_delete_commenti)){
-                    $msgCommenti_delete.='<li class="error">Si è verificato un errori ai nostri servizi, commento dell\'utente '.$userdata[0].' non eliminato</li>';
+                    $msgCommenti_delete.='<li class="error">Si &egrave; verificato un errori ai nostri servizi, commento dell\'utente '.$userdata[0].' non eliminato</li>';
                 }else{
                     $msgCommenti_delete.='<li class="error">Commento dell\'utente '.$userdata[0].' eliminato con successo</li>';
                 }
@@ -199,16 +202,16 @@ if($dbOK){
         $accesso=isset($_POST['cor_accesso'])?$_POST['cor_accesso']:'';
         #controlli sulle variabili
         if (!preg_match('/^(L|LM)[0-9]{2}$/',$classe)){
-            $msgCorso.='<li class="error">La classe di laurea non puo\' essere vuoto o contenere spazi.Le classi di laurea vanno dalla L01 alla L43 e dalla LM01 alla LM94</li>';
+            $msgCorso.='<li class="error">La classe di laurea non pu&ograve; essere vuoto o contenere spazi.Le classi di laurea vanno dalla L01 alla L43 e dalla LM01 alla LM94</li>';
         }
         if (!preg_match('/^(Accesso programmato|Accesso libero con prova|Accesso a numero chiuso|Accesso libero cronologico)$/',$accesso)){
             $msgCorso.='<li class="error">Le modalita di accesso sono Accesso programmato,Accesso libero con prova,Accesso a numero chiuso,Accesso libero cronologico</li>';
         }
         if (!preg_match('/^[a-zA-ZÀ-ÿ\s]{1,50}$/',$ateneo)){
-            $msgCorso.='<li class="error">Il nome dell\'ateneo non puo\' essere vuoto o contenere numeri o caratteri speciali</li>';
+            $msgCorso.='<li class="error">Il nome dell\'ateneo non pu&ograve; essere vuoto o contenere numeri o caratteri speciali</li>';
         }
         if (!preg_match('/^[a-zA-ZÀ-ÿ\s]{1,50}$/',$nome)){
-            $msgCorso.='<li class="error">Il nome del corso di laurea non puo\' essere vuoto o contenere numeri o caratteri speciali</li>';
+            $msgCorso.='<li class="error">Il nome del corso di laurea non pu&ograve; essere vuoto o contenere numeri o caratteri speciali</li>';
         }
         if(!filter_var($link, FILTER_VALIDATE_URL)){
             $msgCorso.='<li class="error">Link non valido, inserire un url corretto</li>';
@@ -236,13 +239,13 @@ if($dbOK){
             $nome=isset($_POST['cor_nome'])?pulisciInput($_POST['cor_nome']):'';
             #controlli sulle variabili
             if (!preg_match('/^(L|LM)[0-9]{2}$/',$classe)){
-                $msgCorso.='<li class="error">La classe di laurea non puo\' essere vuoto o contenere spazi.Le classi di laurea vanno dalla classe L01 alla L43 e dalla LM01 alla LM94</li>';
+                $msgCorso.='<li class="error">La classe di laurea non pu&ograve; essere vuoto o contenere spazi.Le classi di laurea vanno dalla classe L01 alla L43 e dalla LM01 alla LM94</li>';
             }
             if (!preg_match('/^[a-zA-ZÀ-ÿ\s]{1,50}$/',$ateneo)){
-                $msgCorso.='<li class="error">Il nome dell\'ateneo non puo\' essere vuoto o contenere numeri o caratteri speciali</li>';
+                $msgCorso.='<li class="error">Il nome dell\'ateneo non pu&ograve; essere vuoto o contenere numeri o caratteri speciali</li>';
             }
             if (!preg_match('/^[a-zA-ZÀ-ÿ\s]{1,80}$/',$nome)){
-                $msgCorso.='<li class="error">Il nome del corso di laurea non puo\' essere vuoto o contenere numeri o caratteri speciali</li>';
+                $msgCorso.='<li class="error">Il nome del corso di laurea non pu&ograve; essere vuoto o contenere numeri o caratteri speciali</li>';
             }
 
             if(!$msgCorso){
@@ -266,10 +269,10 @@ if($dbOK){
         $rep=PulisciInput($_POST['repepassword']);
         
         if (!preg_match('/^[@a-zA-Z0-9._-]{4,20}$/',$vecchia)){
-            $msgPassword.='<li class="error">Il campo vecchia password non può essere vuoto e non può contenere spazi e deve contenere da 4 a 20 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - )</li>';
+            $msgPassword.='<li class="error">Il campo vecchia password non pu&ograve; essere vuoto e non pu&ograve; contenere spazi e deve contenere da 4 a 20 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - )</li>';
         }
         if (!preg_match('/^[@a-zA-Z0-9._-]{4,20}$/',$nuova)){
-            $msgPassword.='<li class="error">Il campo nuova password non può essere vuoto e non può contenere spazzi e deve contenere da 4 a 20 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - )</li>';
+            $msgPassword.='<li class="error">Il campo nuova password non pu&ograve; essere vuoto e non pu&ograve; contenere spazzi e deve contenere da 4 a 20 caratteri alfanumerici (sono ammessi i seguenti caratteri: @ . _ - )</li>';
         }
         if($nuova!=$rep){
             $msgPassword.='<li class="error">Il campo nuova password e ripeti la password non corrispondono</li>';
